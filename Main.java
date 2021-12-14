764d7dbe51671d611d157a0acf8a94ecbce35ed8
Michael Diramio <56129729+michaeldiramio@users.noreply.github.com>
2021-12-14 18:30:49 +0000

    Initial Setup (that I did in main branch by mistake)

---
+++ Main.java
@@ -0,0 +1,12 @@
+import DLibX.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // for testing
+    DConsole dc = new DConsole(450, 300);
+    dc.fillRect(200, 200, 50, 90);
+    dc.redraw();
+  }
+
+}
