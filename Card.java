af63bc5d6a4a40ccc5f5f4dd660045393ec96d1f
blackbF21 <95363769+brendanb123@users.noreply.github.com>
2021-12-20 15:40:26 +0000

    added boolean "flipped" to card class
    made code in the card class in the printCard method
    
    made code in the card class in the flipCard method

--- Card.java
+++ Card.java
@@ -1,6 +1,10 @@
+import java.util.*;
+import DLibX.*;
+
 public class Card{
   private char suit;
   private int num;
+  private boolean flipped = false;
 
   public Card(char suit, int num){
     this.suit = suit;
@@ -8,10 +12,22 @@ public class Card{
   }
 
   public void flipCard(){
-    
+    if( flipped = true){
+      flipped = false;
+    }else{
+      flipped = true;
+    }
   }
 
-  public void printCard(){
+  public String printCard(){
 
+    if flipped = true{
+      // what file to print
+      String temp = ("Card_Images/" + num + suit + ".png");
+    
+    return temp;
+    }
+    return "Card_Images/card_back.png";
+    
   }
 }
