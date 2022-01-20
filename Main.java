f2d6e64889cce2fcacd5afe634b78b8d48ea3e96
deleonmr <95364420+deleonmr@users.noreply.github.com>
2022-01-20 14:40:21 +0000

    added deck class and tests in main

--- Main.java
+++ Main.java
@@ -3,10 +3,20 @@ import DLibX.*;
 public class Main {
 
   public static void main(String[] args) {
-    // for testing
+    for testing
     DConsole dc = new DConsole(450, 300);
     dc.fillRect(200, 200, 50, 90);
     dc.redraw();
-  }
 
+    
+
+    //deck tests
+    System.out.println("Hello");
+
+    Deck d = new Deck(52);
+    d.shuffle();
+    System.out.println(d.getNumberOfCards());
+    
+  }
+  
 }
