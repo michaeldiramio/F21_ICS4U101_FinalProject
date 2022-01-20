7ab3b238bfec238aa37e9d76db6955cddc2be48e
scotttyler <95363831+SuperW00per@users.noreply.github.com>
2022-01-20 14:51:45 +0000

    re commented out for test

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import DLibX.*;
 public class Main {
 
   public static void main(String[] args) {
-    for testing
+   // for testing
     DConsole dc = new DConsole(450, 300);
     dc.fillRect(200, 200, 50, 90);
     
@@ -33,4 +33,3 @@ public class Main {
   }
   }
 
-}
